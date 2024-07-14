017510640ec100ed6bf4af8d9edc8443069e17c1
pankhudianand <112388880+pankhudianand@users.noreply.github.com>
2024-07-14 10:46:07 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,6 @@
+console.log("pankhudi")
+const accountId=333221
+let emailId="purvi@gmail.com"
+var accountPass="4567"
+accountCity="jaipur"
+console.table([accountId,emailId,accountPass,accountCity]);
