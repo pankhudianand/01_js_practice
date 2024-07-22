e3b38a5909c4ae94742643b532946f6d6b6341b0
pankhudianand <112388880+pankhudianand@users.noreply.github.com>
2024-07-22 16:12:02 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+let name="purvi";
+let age=21
+let isloggedin=false
+console.log(age);
+console.log(name);
+console.log(isloggedin);
+console.table([name,age,isloggedin]);
+console.log(typeof name);
