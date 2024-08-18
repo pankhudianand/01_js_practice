782d2bdf94f9ed5456591f7406390bf24e5a5db5
pankhudianand <112388880+pankhudianand@users.noreply.github.com>
2024-08-18 10:51:50 +0000

    container config done

---
+++ 01_basics/03_type_conversions.js
@@ -0,0 +1,19 @@
+let score1=33;
+console.log(typeof score1);
+let score="33"
+console.log(typeof(score));
+let valInNumber=Number(score);
+console.log(typeof(valInNumber));
+
+
+//if it will not be a number
+let score2="null"
+console.log(typeof(score2));
+let valInNumber2=Number(score2);
+console.log(typeof(valInNumber2));
+console.log(valInNumber2)    //o/p will be NaN means not a number
+
+//"33"=33
+//null=0
+//undefined=nan
+//true=1,false=0
